c87374f05c039cb3c256c9ca39545f46500219db
DanHolmberg1 <dan.holmberg1@hotmail.com>
2023-10-13 17:33:12 +0200

    Realse 0.0.1

--- main.py
+++ main.py
@@ -5,47 +5,35 @@ import math
 pygame.init()
 
 
-WIDTH, HEIGHT = 600, 600
-WHITE = (255, 255, 255)
-JUMPDURATION = 100
-SPEED = 1
-gravityConstant = .5
-failSound = pygame.mixer.Sound('lossSound.mp3')
-timeKeyPressed_W = 0
-timeKeyPressed_UP = 0
-jumpAllowed_W = False
-jumpAllowed_UP = False
-key_wUP = False
-key_upUP = False
-clock = pygame.time.Clock()
-FPS = 240
+WIDTH, HEIGHT = 600, 600 # Width and Height of the window
+WHITE = (255, 255, 255) # The color white
+JUMPDURATION = 150 # Jump duration in game ticks
+SPEED = 1 # Character speed in pixels per game tick
+gravityConstant = .5 # Gravity constant in pixels per game tick
+failSound = pygame.mixer.Sound('lossSound.mp3') # Sound for the fail sound
+timeKeyPressed_W = 0 # Counter for jumpduration in game ticks for key_w
+timeKeyPressed_UP = 0 # Counter for jumpduration in game ticks for key_UP
+jumpAllowed_W = False # Flag for if jumping is allowed for W
+jumpAllowed_UP = False # Flag for if jumping is allowed for UP
+key_wUP = False # Flag for if key_w is realsed
+key_upUP = False # Flag for if key_UP is realsed
+file_path = 'levels.txt' # File path for textfile with all the levels
+clock = pygame.time.Clock() # Used to control the speed of the game
+FPS = 240 # Frames per second (game ticks per second)
 
 
-#Imports Maps
-def read_lists_from_file(filename):
-    with open(filename, 'r') as file:
-        lists = []
-        for line in file:
-            elements = line.strip().split(', ')  # Assuming elements are separated by a comma and a space
-            list_data = [element.strip() for element in elements]
-            lists.append(list_data)
-    return lists
-file_path = 'levels.txt'
-imported_lists = read_lists_from_file(file_path)
-print(imported_lists)
+#########################################################################
 
 
-
-
-
-# Create the game window
-    
+# Create the game window  
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BIGBOYCOOLPROJECT :)")
 
 
+#########################################################################
 
-class Character:
+
+class Character: # A class for the creation of a characters
     def __init__(self, x, y, width, height, color, speed):
         self.rect = pygame.Rect(x, y, width, height) #hitbox
         self.x = x
@@ -58,7 +46,7 @@ class Character:
     def draw(self):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
        
-class Obstacle: 
+class Obstacle: # A class for the creation of a obstacles
     def __init__(self, x, y, width, height, color,trianglePoints):
         self.rect = pygame.Rect(x, y, width, height) # hitbox
         self.trianglePoints = trianglePoints
@@ -74,20 +62,37 @@ class Obstacle:
     def drawHitbox(self):
         pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.width, self.height))
                
-class Platform:
+class Platform: # A class for the creation of a platforms
     def __init__(self, x, y, width, height, color):
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
 
+
+#########################################################################
+
+
 level_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.75, 2.5, 2.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.75, 2.0, 2.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.25, 1.25, 
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.0, 2.0, 2.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 2.75, 0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.75, 0, 0, 0, 0, 0, 0, 0, 1.0, 2.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 0, 0, 0, 0, 0, 1.0, 2.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 2.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.25, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.25, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.0, 2.0, 2.0, 2.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.0, 2.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-0, 0, 0, 0, 0, 0, 2.0, 0, 0, 0, 0, 0, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+0, 0, 0, 0, 0, 0, 2.0, 0, 0, 0, 0, 0, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0] # A single game level
 
 
-def makeLevel(level):
+#########################################################################
+
+
+def read_lists_from_file(filename): # Reads levels.txt to load maps
+    with open(filename, 'r') as file:
+        lists = []
+        for line in file:
+            elements = line.strip().split(', ')  # Assuming elements are separated by a comma and a space
+            list_data = [element.strip() for element in elements]
+            lists.append(list_data)
+    return lists 
+
+
+def makeLevel(level): # Ask David
     cnt = 0
     width = 30
     height = 30
@@ -132,14 +137,21 @@ def makeLevel(level):
             cnt += 1
 
 
-#Obstacles      
-Obstacles = []
-#Charaters    
-Characters = [Character(50, 500, 20, 20, (255, 0, 0), SPEED), Character(20, 500, 20, 20 , (255, 0, 0), SPEED)]
-#Platforms
-Platforms = []
+#########################################################################
 
-def checkKeyUp():
+      
+Obstacles = [] # A list of all the obstacles
+
+    
+Characters = [Character(50, 500, 20, 20, (255, 0, 0), SPEED), Character(20, 500, 20, 20 , (255, 0, 0), SPEED)] # A list of all the characters
+
+
+Platforms = [] # A list of all the platforms
+
+#########################################################################
+
+
+def checkKeyUp(): # Function that checks if a key is realesed and if so block if from being pressed (mitigated by gravity and platformCollision to allow jumping if on ground or platform collision)
     keys = pygame.key.get_pressed()
     global key_wUP
     global key_upUP
@@ -148,8 +160,12 @@ def checkKeyUp():
     if not keys[pygame.K_UP]:
         key_upUP = True
 
-#Function that handles collision detection between platforms and characters        
-def platformCollision(character, platform):
+
+#########################################################################
+
+
+        
+def platformCollision(character, platform): # Function that handles collision detection between platforms and characters
     if character.rect.bottom == platform.rect.top and platform.rect.left < character.rect.x+character.width and platform.rect.right > character.rect.x:
         #print("yes")
         character.y -= gravityConstant
@@ -178,89 +194,10 @@ def platformCollision(character, platform):
         if character.rect.y+character.height > platform.rect.top and character.rect.y < platform.rect.bottom:            
             character.x -= character.speed
             character.rect.x = character.x
-               
-#Function for testing collision and plays a sound if you collided and then exits the game
-def collision(character, obj):
-    if character.rect.colliderect(obj.rect):
-        failSound.play()
-        pygame.time.delay(int(failSound.get_length()*1000))
-        global running 
-        running = False
-        
-#Handle character move0ment WASD    
-def move0(character):
-    keys = pygame.key.get_pressed()
-    global timeKeyPressed_W
-    
-    if keys[pygame.K_d] and character.x < WIDTH-character.width:
-        character.x += character.speed
-        character.rect.x = character.x
-    if keys[pygame.K_a] and character.x > 0:
-        character.x -= character.speed
-        character.rect.x = character.x
-    if keys[pygame.K_w] and character.y > 0 and jumpAllowed_W == True and key_wUP == False:
-        timeKeyPressed_W += 1
-        print(timeKeyPressed_W)
-         # funkar inte men 
 
-        if timeKeyPressed_W < JUMPDURATION:
-            character.y -= character.speed+.15
-            character.rect.y = character.y   
-   # if keys[pygame.K_s] and character.y < HEIGHT - character.height: Downward movment if you want to be able to move0 through a platform and speed down.
-   #     character.y += character.speed
-   #     character.rect.y = character.y
-   
-#Handle charater movment arrow keys
-def move01(character):
-    keys = pygame.key.get_pressed()
-    global timeKeyPressed_UP
-    if keys[pygame.K_RIGHT] and character.x < WIDTH-character.width:
-        character.x += character.speed
-        character.rect.x = character.x
-    if keys[pygame.K_LEFT] and character.x > 0:
-        character.x -= character.speed
-        character.rect.x = character.x
-    if keys[pygame.K_UP] and character.y > 0 and jumpAllowed_UP == True and key_upUP == False:
-        timeKeyPressed_UP += 1
-        print(timeKeyPressed_UP)
-         # funkar inte men 
 
-        if timeKeyPressed_UP < JUMPDURATION:
-            character.y -= character.speed+.15
-            character.rect.y = character.y
-    #if keys[pygame.K_DOWN] and character.y < HEIGHT - character.height:
-     #       character.y += character.speed
-      #      character.rect.y = character.y
-def reset0(reset):
-    if reset == True:
-        global timeKeyPressed_W
-        timeKeyPressed_W = 0
-        
-def reset1(reset):
-    if reset == True:
-        global timeKeyPressed_UP
-        global key_upUP
-        timeKeyPressed_UP = 0
-        key_upUP = False
 
-def canJump0(canJump):
-    global jumpAllowed_W
-    global key_wUP
-    if canJump == False and timeKeyPressed_W == 0:
-        jumpAllowed_W = False
-        key_wUP = False
-    else:
-        jumpAllowed_W = True
-def canJump1(canJump):
-    global jumpAllowed_UP
-    if canJump == False and timeKeyPressed_UP == 0:
-        jumpAllowed_UP = False
-    else:
-        jumpAllowed_UP = True
-
-        
-#Functions that handles gravity move0ment
-def gravity(character):
+def gravity(character): # Functions that handles gravity move0ment
     if character.y < HEIGHT - character.height:
         character.y += gravityConstant
         character.rect.y = character.y
@@ -278,7 +215,99 @@ def gravity(character):
         if character == Characters[1]:
             canJump1(True)
 
-makeLevel(level_1)
+
+#########################################################################
+
+
+
+def collision(character, obj): # Function for testing collision and plays a sound if you collided and then exits the game
+    if character.rect.colliderect(obj.rect):
+        failSound.play()
+        pygame.time.delay(int(failSound.get_length()*1000))
+        global running 
+        running = False
+
+
+#########################################################################
+
+
+ 
+def move0(character): # Handle character move0ment WASD   
+    keys = pygame.key.get_pressed()
+    global timeKeyPressed_W
+    
+    if keys[pygame.K_d] and character.x < WIDTH-character.width:
+        character.x += character.speed
+        character.rect.x = character.x
+    if keys[pygame.K_a] and character.x > 0:
+        character.x -= character.speed
+        character.rect.x = character.x
+    if keys[pygame.K_w] and character.y > 0 and jumpAllowed_W == True and key_wUP == False:
+        timeKeyPressed_W += 1
+        #print(timeKeyPressed_W) 
+
+        if timeKeyPressed_W < JUMPDURATION:
+            character.y -= character.speed+.15
+            character.rect.y = character.y   
+
+
+
+def move01(character): # Handle charater movment arrow keys
+    keys = pygame.key.get_pressed()
+    global timeKeyPressed_UP
+    if keys[pygame.K_RIGHT] and character.x < WIDTH-character.width:
+        character.x += character.speed
+        character.rect.x = character.x
+    if keys[pygame.K_LEFT] and character.x > 0:
+        character.x -= character.speed
+        character.rect.x = character.x
+    if keys[pygame.K_UP] and character.y > 0 and jumpAllowed_UP == True and key_upUP == False:
+        timeKeyPressed_UP += 1
+        #print(timeKeyPressed_UP)
+
+
+        if timeKeyPressed_UP < JUMPDURATION:
+            character.y -= character.speed+.15
+            character.rect.y = character.y
+
+
+def reset0(reset): # resets the counter for jumping for the character with WASD controlls
+    if reset == True:
+        global timeKeyPressed_W
+        timeKeyPressed_W = 0
+
+
+def reset1(reset): # resets the counter for jumping for the character with arrow keys controlls
+    if reset == True:
+        global timeKeyPressed_UP
+        global key_upUP
+        timeKeyPressed_UP = 0
+        key_upUP = False
+
+
+def canJump0(canJump): # Checks if the character can jump WASD controlls
+    global jumpAllowed_W
+    global key_wUP
+    if canJump == False and timeKeyPressed_W == 0:
+        jumpAllowed_W = False
+        key_wUP = False
+    else:
+        jumpAllowed_W = True
+def canJump1(canJump): # Checks if the character can jump arrow keys controlls
+    global jumpAllowed_UP
+    if canJump == False and timeKeyPressed_UP == 0:
+        jumpAllowed_UP = False
+    else:
+        jumpAllowed_UP = True
+
+
+#########################################################################        
+
+
+imported_lists = read_lists_from_file(file_path)
+makeLevel(level_1) # Loads the level
+# print(imported_lists) Test to see that the levels load correctly
+
 
 running = True
 while running: # Main loop
@@ -287,27 +316,48 @@ while running: # Main loop
         if event.type == pygame.QUIT:
             running = False
 
+
+#########################################################################
+
+
     #Calls function that handles movment events
     move0(Characters[0])
     move01(Characters[1])
-    
-    #Calls function that handles gravity
-    for i in range(len(Characters)):
-        gravity(Characters[i])
-        
-    
+    checkKeyUp()
+
+
+#########################################################################   
+
+
     #Calls function that handles collision
     for o in range(len(Characters)):
         for i in range(len(Obstacles)):
             collision(Characters[o], Obstacles[i])
-            
+
+
+#########################################################################
+
+
+    #Calls function that handles gravity
+    for i in range(len(Characters)):
+        gravity(Characters[i])
+
+
     #Calls function that handles Platform collision
     for o in range(len(Characters)):
         for i in range(len(Platforms)):
             platformCollision(Characters[o], Platforms[i])
-    checkKeyUp()
+
+
+#########################################################################  
+ 
+  
     # Clear the screen
     screen.fill(WHITE)
+
+
+#########################################################################
+
 
     # Draw the character & obstacles & platforms
     for i in range(len(Characters)):
@@ -319,7 +369,10 @@ while running: # Main loop
     
     for i in range(len(Platforms)):
         Platforms[i].draw()
-    
+
+
+#########################################################################
+
     # Update the display
     pygame.display.update()
 
